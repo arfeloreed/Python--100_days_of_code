7f18f6a979a03d9bba2bece432af56c1846b12aa
mr-fravardin <116253120+mr-fravardin@users.noreply.github.com>
2022-11-15 18:59:38 +0800

    Mini project for day_03

---
+++ day_03/love_calculator.py
@@ -0,0 +1,38 @@
+# love calculator
+
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+# converting the names to lower case
+name1 = name1.lower()
+name2 = name2.lower()
+
+# counting the letters in true
+t = name1.count("t") + name2.count("t")
+r = name1.count("r") + name2.count("r")
+u = name1.count("u") + name2.count("u")
+e = name1.count("e") + name2.count("e")
+true = t + r + u + e
+
+# counting the letters in love
+l = name1.count("l") + name2.count("l")
+o = name1.count("o") + name2.count("o")
+v = name1.count("v") + name2.count("v")
+e = name1.count("e") + name2.count("e")
+love = l + o + v + e
+
+# calculate score
+score = str(true) + str(love)   # or score = int(str(true) + str(love))
+score = int(score)
+
+# interpretations
+if score < 10 or score > 90:
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif score >= 40 and score <= 50:
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}.")
