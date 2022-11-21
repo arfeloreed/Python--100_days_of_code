1370abcd11dc33fe68482944651d8597c4ec0df6
mr-fravardin <116253120+mr-fravardin@users.noreply.github.com>
2022-11-21 21:07:33 +0800

    main file of the project
    Amended due to offset in coordinates in the initial coding.

--- project_codes/day_22_pong_game/main.py
+++ project_codes/day_22_pong_game/main.py
@@ -4,22 +4,25 @@ import paddle as p
 import ball as b
 import score as s
 
+WIDTH, HEIGHT = 800, 600
+
 my_screen = t.Screen()
 my_screen.tracer(0)
 my_screen.title("Pong Game")
 my_screen.bgcolor("black")
-my_screen.screensize(canvwidth=800, canvheight=600)
+my_screen.screensize(canvwidth=WIDTH, canvheight=HEIGHT)
+my_screen.setup(width=WIDTH+2, height=HEIGHT+2)
 
 # create a paddle
-r_paddle = p.Paddle((450, 0))
-l_paddle = p.Paddle((-450, 0))
+r_paddle = p.Paddle((380, 0))
+l_paddle = p.Paddle((-380, 0))
 # create a game over turtle for notification
 over_object = t.Turtle()
 over_object.penup()
 over_object.hideturtle()
 # create a score object
-r_score = s.Scoreboard((30, 370))
-l_score = s.Scoreboard((-30, 370))
+r_score = s.Scoreboard((30, 260))
+l_score = s.Scoreboard((-30, 260))
 
 # create ball
 ball = b.Ball((0, 0))
@@ -37,16 +40,16 @@ while True:
     ball.ball_moving()
     my_screen.update()
     # detecting collision with top and bottom wall
-    if ball.ycor() == 380 or ball.ycor() == -380:
+    if ball.ycor() == 280 or ball.ycor() == -280:
         ball.ball_wall_bounce()
     # detect collision with paddles
-    if (ball.distance(r_paddle) < 80 and ball.xcor() == 430) or (ball.distance(l_paddle) < 80 and ball.xcor() == -430):
+    if (ball.distance(r_paddle) < 80 and ball.xcor() == 350) or (ball.distance(l_paddle) < 80 and ball.xcor() == -350):
         ball.ball_paddle_bounce()
     # detect if ball was out of bounce and count score
-    if ball.xcor() > 460:
+    if ball.xcor() > 390:
         ball.ball_switch()
         l_score.score_plus()
-    elif ball.xcor() < -460:
+    elif ball.xcor() < -390:
         ball.ball_switch()
         r_score.score_plus()
 
